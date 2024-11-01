73454d1c55789c2b877e8610246c032e3166cf02
Yasinvalipoor <yasinvalipoor14@gmail.com>
2024-11-01 17:19:34 +0330

    animate

--- assets/Scripts/customjs.js
+++ assets/Scripts/customjs.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // Hover
     $("#box").hover(
         function () {
             // over
@@ -10,17 +11,47 @@ $(document).ready(function () {
         }
     );
 
+    // Use multi Events
     $("#box").on({
-        mouseenter : function () { 
-            console.log("موس وارد جعبه شد");
+        mouseenter: function () { // console.log("موس وارد جعبه شد");
         },
-        
-        mouseout : function () { 
-            console.log("موس از جعبه خارج شد");
-        }
+
+        mouseout: function () { // console.log("موس از جعبه خارج شد"); 
+        },
+
+        // Use Our Function
+        mousemove: informationMouse
+
     });
 
-    $().m
+    // Write Function - Js , jquery
+    function informationMouse(event) {
+        var xAxis = event.pageX;
+        var yAxis = event.pageY;
+        $("#showInfo").text(`X-axis Mouse is : ${xAxis} & Y-axis Mouse is : ${yAxis}`);
+    }
 
-    
+    $("#click_for_fade").click(function () {
+        // FadeIn + FadeOut = fadeToggle
+        $(".square_1").fadeToggle(1250);
+        $(".square_2").fadeToggle(1500);
+        $(".square_3").fadeToggle(1750);
+        $(".square_4").fadeToggle(2000);
+    });
+
+    $("#click_for_opacity").click(function () {
+        // FadeTo For Opacity
+        $(".square_1").fadeTo("slow", 1);
+        $(".square_2").fadeTo("slow", 0.75);
+        $(".square_3").fadeTo("slow", 0.5);
+        $(".square_4").fadeTo("slow", 0.25);
+    });
+
+    $(".btn_panel").click(function () {
+        $(".panel").slideToggle("slow");
+    });
+
+    $("#accordion-heading").click(function () {
+        $("#accordion-content").slideToggle(400);
+    });
 });
